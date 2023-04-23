8475b1a589713a13ef2a5a9adf561f91ee96f19f
reasu <reasu@protonmail.com>
2023-04-23 22:17:16 +0200

    ende des Abends
    commit 46123ec96a8f08bd58e7de973e34d117a072b7d4
    Author: reasu <reasu@protonmail.com>
    Date:   Sun Apr 23 22:17:08 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:java/src/main/java/Ocr.java

--- java/src/main/java/Ocr.java
+++ java/src/main/java/Ocr.java
@@ -3,76 +3,67 @@ import java.util.ArrayList;
 
 public class Ocr {
 
-    private static final char[][][] NUMERALS = new char[][][] {
-        {" _  ".toCharArray(),
-         "| | ".toCharArray(),
-         "|_| ".toCharArray(),
-         "    ".toCharArray()},
-        {"    ".toCharArray(),
-         "  | ".toCharArray(),
-         "  | ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         " _| ".toCharArray(),
-         "|_  ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         " _| ".toCharArray(),
-         " _| ".toCharArray(),
-         "    ".toCharArray()},
-        {"    ".toCharArray(),
-         "|_| ".toCharArray(),
-         "  | ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         "|_  ".toCharArray(),
-         " _| ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         "|_  ".toCharArray(),
-         "|_| ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         "  | ".toCharArray(),
-         "  | ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         "|_| ".toCharArray(),
-         "|_| ".toCharArray(),
-         "    ".toCharArray()},
-        {" _  ".toCharArray(),
-         "|_| ".toCharArray(),
-         " _| ".toCharArray(),
-         "    ".toCharArray()}};
-
     public static List<String> parse(String... lines) {
+        // TODO iterate over input bank account number
         final List<String> result = new ArrayList<String>();
         for (int i = 0; i < lines.length; i += 4) {
-            final char[] work = "             ".toCharArray();
-            for (int pos = 0; pos < 9; ++pos) {
-                work[pos] = '?';
-                boolean got1 = false;
-                for (int numeral = 0; numeral <= 9; ++numeral) {
-                    boolean ok = true;
-                    for (int row = 0; row < 4; ++row) {
-                        for (int col = 0; col < 4; ++col) {
-                            if (NUMERALS[numeral][row][col] != lines[i+row].charAt(4*pos+col))
-                                ok = false;
-                        }
-                    }
-                    if (ok) {
-                        work[pos] = (char)(numeral + (int)'0');
-                        got1 = true;
-                        break;
-                    }
-                }
-                if (!got1) {
-                    work[10] = 'I';
-                    work[11] = work[12] = 'L';
-                }
-            }
-            result.add(new String(work));
+            analiseOcrNumber(result, i, lines);
         }
         return result;
     }
+
+    private static void analiseOcrNumber(List<String> result, int i, String[] lines) {
+        final char[] work = "             ".toCharArray();
+        for (int pos = 0; pos < 9; ++pos) {
+            compare_singleNumberFromInput_withNumerals(i, lines, work, pos);
+        }
+        result.add(new String(work));
+    }
+
+    private static void compare_singleNumberFromInput_withNumerals(int i, String[] lines, char[] work, int pos) {
+        // TODO vergleicht einzelne Zahlen aus dem Input mit NUMERALS
+        work[pos] = '?';
+        for (int numeral = 0; numeral <= 9; ++numeral) {
+            boolean ok = foo(i, lines, pos, numeral);
+            // TODO wenn alle chars gleich waren (= keine chars unterschiedlich waren),
+            //  wird die gefundene Ziffer an die position von work gespeichert.
+            if (ok) {
+                work[pos] = foo2(i, lines, pos, numeral);
+            }
+        }
+        if (work[pos] == '?') {
+            work[10] = 'I';
+            work[11] = work[12] = 'L';
+        }
+    }
+
+    private static boolean foo(int i, String[] lines, int pos, int numeral) {
+        boolean ok = true;
+        // TODO iteriert über die 4 Zeilen von Numeral bzw. lines
+        for (int row = 0; row < 4; ++row) {
+            // TODO iteriert über die 4 chars jeder Zeile von Numeral bzw. lines
+            for (int col = 0; col < 4; ++col) {
+                // TODO wenn die chars ungleich sind, wird ok auf false gesetzt
+                if (Numerals.NUMERALS[numeral][row][col] != lines[i +row].charAt(4* pos +col))
+                    ok = false;
+            }
+        }
+        return ok;
+    }
+
+    private static char foo2(int i, String[] lines, int pos, int numeral) {
+        boolean ok = true;
+        // TODO iteriert über die 4 Zeilen von Numeral bzw. lines
+        for (int row = 0; row < 4; ++row) {
+            // TODO iteriert über die 4 chars jeder Zeile von Numeral bzw. lines
+            for (int col = 0; col < 4; ++col) {
+                // TODO wenn die chars ungleich sind, wird ok auf false gesetzt
+                if (Numerals.NUMERALS[numeral][row][col] != lines[i +row].charAt(4* pos +col))
+                    ok = false;
+            }
+        }
+        char numeralString = Character.forDigit(numeral, 10);
+        return ok ? numeralString : '?';
+    }
+
 }
