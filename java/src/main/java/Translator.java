1549b218c16ca734bd2205a14f3b4c00a40be148
reasu <reasu@protonmail.com>
2023-07-08 12:31:56 +0200

    put that new class in its own file

--- java/src/main/java/Translator.java
+++ java/src/main/java/Translator.java
@@ -45,32 +45,3 @@ class Translator {
 	}
 
 }
-
-class Result {
-	StringBuilder sb;
-
-	Result() {
-		sb = new StringBuilder();
-	}
-
-	void add(String next) {
-		sb.append(next);
-	}
-
-	void addMarker() {
-		String marker = "    ";
-		if (isIllegal(sb)) {
-			marker = " ILL";
-		}
-		sb.append(marker);
-	}
-
-	@Override
-	public String toString() {
-		return sb.toString();
-	}
-
-	private boolean isIllegal(StringBuilder sb) {
-		return sb.indexOf("?") > -1;
-	}
-}
